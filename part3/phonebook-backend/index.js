445faf55ac768956601f8e3bd1036409870446a7
Ijtihed <ijtihedk@gmail.com>
2025-06-21 19:37:00 +0300

    3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -24,6 +24,8 @@ let persons = [
   }
 ];
 
+app.use(express.json())
+
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
@@ -49,6 +51,23 @@ app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter(person => person.id !== id);
   res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' });
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
 });
 
 const PORT = 3001;
