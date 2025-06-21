1c11c85154350ade33e68bd95f939c138139f54d
Ijtihed <ijtihedk@gmail.com>
2025-06-21 16:35:41 +0300

    Exercise 1.6

--- part1/part1-courseinfo/src/App.jsx
+++ part1/part1-courseinfo/src/App.jsx
@@ -1,54 +1,17 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
-  return <p>Number of exercises {total}</p>
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+
+      <h2>statistics</h2>
+      <p>good {good}</p>
     </div>
   )
 }
