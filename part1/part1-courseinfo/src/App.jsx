31f4a672e359cfb26b15cec2d3a0f2e3c0b0af5a
Ijtihed <ijtihedk@gmail.com>
2025-06-21 16:43:27 +0300

    Exercise 1.10

--- part1/part1-courseinfo/src/App.jsx
+++ part1/part1-courseinfo/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average.toFixed(1)}</p>
+      <p>positive {positive.toFixed(1)}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive = total === 0 ? 0 : (good / total) * 100
 
   return (
     <div>
@@ -16,13 +30,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average.toFixed(1)}</p>
-      <p>positive {positive.toFixed(1)}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
